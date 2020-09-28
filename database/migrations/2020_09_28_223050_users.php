b57c14a7801c59c1437408ec70d4d4ec3a0cf729
AdrianoLG <adri@adri.info>
2020-09-28 22:58:25 +0200

    Users and groups tables

---
+++ database/migrations/2020_09_28_223050_users.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Users extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->tinyIncrements('id');
+            $table->string('name', 30);
+            $table->string('email', 50)->unique();
+            $table->string('password', 30);
+            $table->boolean('shared_comments');
+            $table->boolean('shared_ratings');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
