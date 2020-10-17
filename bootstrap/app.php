7f8d4b43475150d69479cbfda07a1aa3707cfe8c
AdrianoLG <adri@adri.info>
2020-10-18 00:02:33 +0200

    Passport authentication

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,6 +23,10 @@ $app = new Laravel\Lumen\Application(
     dirname(__DIR__)
 );
 
+$app = new \Dusterio\LumenPassport\Lumen7Application(
+    dirname(__DIR__)
+);
+
 $app->withFacades();
 
 $app->withEloquent();
@@ -60,6 +64,10 @@ $app->singleton(
 */
 
 $app->configure('app');
+$app->configure('auth');
+
+\Dusterio\LumenPassport\LumenPassport::allowMultipleTokens();
+\Dusterio\LumenPassport\LumenPassport::tokensExpireIn(\Carbon\Carbon::now()->addHour());
 
 /*
 |--------------------------------------------------------------------------
@@ -76,9 +84,9 @@ $app->configure('app');
 //     App\Http\Middleware\ExampleMiddleware::class
 // ]);
 
-// $app->routeMiddleware([
-//     'auth' => App\Http\Middleware\Authenticate::class,
-// ]);
+$app->routeMiddleware([
+    'auth' => App\Http\Middleware\Authenticate::class,
+]);
 
 /*
 |--------------------------------------------------------------------------
@@ -95,6 +103,8 @@ $app->configure('app');
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 $app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
+$app->register(Laravel\Passport\PassportServiceProvider::class);
+$app->register(Dusterio\LumenPassport\PassportServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
@@ -106,6 +116,7 @@ $app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
 | can respond to, as well as the controllers that may handle them.
 |
 */
+\Dusterio\LumenPassport\LumenPassport::routes($app);
 
 $app->router->group([
     'namespace' => 'App\Http\Controllers',
--- routes/web.php
+++ routes/web.php
@@ -20,26 +20,30 @@ $router->get('/', function () use ($router) {
 $router->group(['prefix' => '/api'], function () use ($router) {
     $router->group(['prefix' => '/user'], function () use ($router) {
         $router->post('/', 'UserController@createUser');
-        $router->get('/{id}', 'UserController@getUser');
         $router->put('/password/{id}', 'UserController@updateUserPassword');
-        $router->put('/password-token', 'UserController@updateUserPasswordToken');
-        $router->put('/reset-password-token', 'UserController@resetUserPasswordToken');
-        $router->delete('/{id}', 'UserController@removeUser');
+        $router->group(['middleware' => 'auth'], function () use ($router) {
+            $router->put('/password-token', 'UserController@updateUserPasswordToken');
+            $router->put('/reset-password-token', 'UserController@resetUserPasswordToken');
+            $router->get('/{id}', 'UserController@getUser');
+            $router->delete('/{id}', 'UserController@removeUser');
+        });
     });
-    $router->get('/books', 'BookController@getBooks');
-    $router->group(['prefix' => '/book'], function () use ($router) {
-        $router->post('/', 'BookController@createBook');
-        $router->put('/{id}', 'BookController@updateBook');
-        $router->delete('/{id}', 'BookController@removeBook');
-    });
-    $router->get('/groups', 'GroupController@getGroups');
-    $router->group(['prefix' => '/group'], function () use ($router) {
-        $router->post('/', 'GroupController@createGroup');
-        $router->post('/user', 'GroupController@createGroupUser');
-        $router->get('/{id}', 'GroupController@getGroup');
-        $router->put('/{id}', 'GroupController@updateGroup');
-        $router->put('/user/{group_user_id}', 'GroupController@updateGroupUser');
-        $router->delete('/{id}', 'GroupController@removeGroup');
-        $router->delete('/user/{id}', 'GroupController@removeGroupUser');
+    $router->group(['middleware' => 'auth'], function () use ($router) {
+        $router->get('/books', 'BookController@getBooks');
+        $router->group(['prefix' => '/book'], function () use ($router) {
+            $router->post('/', 'BookController@createBook');
+            $router->put('/{id}', 'BookController@updateBook');
+            $router->delete('/{id}', 'BookController@removeBook');
+        });
+        $router->get('/groups', 'GroupController@getGroups');
+        $router->group(['prefix' => '/group'], function () use ($router) {
+            $router->post('/', 'GroupController@createGroup');
+            $router->post('/user', 'GroupController@createGroupUser');
+            $router->get('/{id}', 'GroupController@getGroup');
+            $router->put('/{id}', 'GroupController@updateGroup');
+            $router->put('/user/{group_user_id}', 'GroupController@updateGroupUser');
+            $router->delete('/{id}', 'GroupController@removeGroup');
+            $router->delete('/user/{id}', 'GroupController@removeGroupUser');
+        });
     });
 });
